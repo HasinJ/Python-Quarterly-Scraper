2aa949a5db4448c682d972578587ce18d7a7b4d1
HasinJ <39993584+HasinJ@users.noreply.github.com>
2020-08-14 14:24:05 -0400

    directory and priv variables

--- config.py
+++ config.py
@@ -1,4 +1,14 @@
 class config():
     def __init__(self):
-        self.radUser = ''
-        self.radPass = ''
+        self.__radUser = StringVar()
+        self.__radPass = StringVar()
+        self.__directory = StringVar()
+
+    def getDirectory(self):
+        return self.directory
+
+    def getRadUser(self):
+        return self.__radUser
+
+    def getRadPass(self):
+        return self.__radPass
