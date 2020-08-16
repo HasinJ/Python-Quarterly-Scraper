73d49dfd6c2cce4bd2e5524f27cf9dc22151fd54
HasinJ <39993584+HasinJ@users.noreply.github.com>
2020-08-15 20:42:30 -0400

    driver switches back to report options

--- main.py
+++ main.py
@@ -40,6 +40,7 @@ class sqlQueries(config):
             except MySQLdb._exceptions.IntegrityError:
                 print(f'PC Number: {pcNumber} exists in database.')
                 continue
+        print('\n')
 
 
 class Radiant(config):
@@ -80,7 +81,6 @@ class Radiant(config):
         self.odd = self.driver.find_elements_by_class_name('gridRowOdd')
         self.even = self.driver.find_elements_by_class_name('gridRowEven')
         elements = self.even + self.odd #for selenium to click
-        print(elements)
         time.sleep(1)
 
         for elementsIndex in range(len(elements)):
@@ -89,18 +89,64 @@ class Radiant(config):
                 continue
             elif somePC == self.pcNumbers[0]:
                 firstPC = elements[elementsIndex]
-                print(somePC + ' is the first PC number AKA Business Unit')
+                print(f'\n{somePC} is the first PC number AKA Business Unit')
                 self.pcNumbers.remove(somePC)
                 self.oddCount = 1
                 break
 
         ActionChains(self.driver).move_to_element(firstPC).click(firstPC).perform()
+        time.sleep(1)
+        self.backToReportOptions()
+
+    def backToReportOptions(self, **kwargs): #from popup
+        print('Switching driver focus back to report options...')
+
+        #click on save and close
+        for key, value in kwargs.items():
+            if key == 'closeID':
+                setWait(self.driver, 5)
+                saveANDclose = self.getWait().until(EC.presence_of_element_located((By.ID, closeID)))
+                saveANDclose.send_keys(Keys.ENTER)
+                break
+            elif len(kwargs) > 1:
+                print('too many arguments for backToReportOptions()')
+                self.driver.quit()
+                exit()
+
+        #go back to previous screen/frame
+        self.driver.switch_to.window(self.main_page)
+        self.driver.switch_to.default_content()
+
+        frame = self.getWait().until(EC.presence_of_element_located((By.ID, "fraContent"))) #stays the same in report options screen
+        self.driver.switch_to.frame(frame)
+
+        frame = self.getWait().until(EC.presence_of_element_located((By.ID, 'Frame2'))) #stays the same in report options screen
+        self.driver.switch_to.frame(frame)
+        print('Switched. \n')
+        print(self.driver.page_source)
+
+    def handlePCNumbers(self):
+        wait = WebDriverWait(self.driver, 3)
+        frame = wait.until(EC.presence_of_element_located((By.ID, 'renderFrame'))) #frame inside the modal box
+        self.driver.switch_to.frame(frame)
+        time.sleep(1)
+
+        soup = BeautifulSoup(self.driver.page_source,'html.parser')
+        table = soup.find(id="grdHierarchy")
+        rows = table.findAll(True, {'class':['gridRowOdd', 'gridRowEven']})
+
+        for index in range(len(rows)):
+            dataCell = rows[index].find(class_='gridCell')
+            self.pcNumbers.insert(index, dataCell.text.strip())
+        if len(rows) == len(self.pcNumbers):
+            time.sleep(1) #it'll wait once the pcNumbers are saved into pcNumbers array
 
     def setWait(self,driver,time): #needs to be set everytime driver changes
         self.__wait = WebDriverWait(driver,time)
 
     def getWait(self):
         return self.__wait
+
 
 class QuarterlyHour(Radiant): #can use parent variables by just calling it
     def __init__(self,driver):
@@ -125,26 +171,11 @@ class QuarterlyHour(Radiant): #can use parent variables by just calling it
         self.driver.switch_to.frame(frame)
         self.setWait(self.driver,5) #needs to be set everytime driver changes
 
-    def handlePCNumbers(self):
-        wait = WebDriverWait(self.driver, 3)
-        frame = wait.until(EC.presence_of_element_located((By.ID, 'renderFrame'))) #frame inside the modal box
-        self.driver.switch_to.frame(frame)
-        time.sleep(1)
-
-        soup = BeautifulSoup(self.driver.page_source,'html.parser')
-        table = soup.find(id="grdHierarchy")
-        rows = table.findAll(True, {'class':['gridRowOdd', 'gridRowEven']})
-
-        for index in range(len(rows)):
-            dataCell = rows[index].find(class_='gridCell')
-            self.pcNumbers.insert(index, dataCell.text.strip())
-        if len(rows) == len(self.pcNumbers):
-            time.sleep(1) #it'll wait once the pcNumbers are saved into pcNumbers array
-
 
 class DDailySummary(Radiant):
     def __init__(self,driver):
         super().__init__(driver)
+
 
 #polymorphism
 def switchHandle(currentDriver): #to popup
@@ -165,26 +196,16 @@ def switchHandle(currentDriver): #to popup
     print('Switched. \n')
     return main_page
 
-#can move to Radiant class and method override when it comes to DDailySummary
-def backToReportOptions(currentDriver, main_page, wait, closeID='nothing'): #from popup
-    print('Switching driver focus back to report options...')
-
-    #click on save and close
-    if closeID != 'nothing':
-        wait = WebDriverWait(currentDriver,waitingTime)
-        saveANDclose = wait.until(EC.presence_of_element_located((By.ID, closeID)))
-        saveANDclose.send_keys(Keys.ENTER)
-
-    #go back to previous screen/frame
-    currentDriver.switch_to.window(main_page)
-    currentDriver.switch_to.default_content()
-
-    frame = wait.until(EC.presence_of_element_located((By.ID, "fraContent"))) #stays the same in report options screen
-    currentDriver.switch_to.frame(frame)
-
-    frame = wait.until(EC.presence_of_element_located((By.ID, 'Frame2'))) #stays the same in report options screen
-    currentDriver.switch_to.frame(frame)
-
+def get_past_date(str_days_ago):
+    TODAY = datetime.date.today()
+    splitted = str_days_ago.split()
+    if len(splitted) == 1 and splitted[0].lower() == 'today':
+        return TODAY
+    elif len(splitted) == 1 and splitted[0].lower() == 'yesterday':
+        date = TODAY - relativedelta(days=1)
+        return date
+    else:
+        return "Wrong Argument format"
 
 
 if __name__=="__main__":
